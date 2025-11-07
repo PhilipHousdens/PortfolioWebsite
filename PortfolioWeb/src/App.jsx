b46d23395b5e4e62977c4aa4b59eebd94c441c5c
Philip Housden <philip.houden.04@gmail.com>
2025-11-07 19:23:39 +0700

    initial commit

---
+++ PortfolioWeb/src/App.jsx
@@ -0,0 +1,17 @@
+import './styles.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className=' text-5xl pt-20 text-center text-yellow-900'>Pong Dumb FUCK</h1>
+        <div className='mx-auto'>
+            <img src="/monkey-pic.jpeg" alt="pong-image" className='w-[50%] mx-auto mt-5' />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
