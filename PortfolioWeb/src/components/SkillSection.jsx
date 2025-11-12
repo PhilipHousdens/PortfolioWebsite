6a6b37762cd32b4ed6f367cbc8f57d1796f25812
PhilipHousdens <philip.houden.04@gmail.com>
2025-11-12 20:48:26 +0700

    adding tech image into the skills page

--- PortfolioWeb/src/components/SkillSection.jsx
+++ PortfolioWeb/src/components/SkillSection.jsx
@@ -1,8 +1,33 @@
 function SkillSection() {
     return (   
         <section className="h-screen flex flex-col justify-center items-center text-white relative overflow-hidden">
-        <div className="flex justify-center items-center h-full">
-            <h2 className="text-5xl font-bold underline">Skills Section Coming Soon!</h2>
+        <div className="h-full">
+            <h2 className="text-5xl text-center mt-25 font-bold underline">SKILLS</h2>
+            <br/>
+            <div className="mt-10 flex flex-col gap-20 text-2xl ">
+                {/* Front End Section */}
+                <div>
+                    <p>Front-End</p>
+                    <ul className="text-lg flex justify-between gap-15 mt-5">
+                        <li className="flex-col justify-center items-center flex"><img src="../../public/HTML5_logo_and_wordmark.svg.png" alt="" className="h-[95px]" />HTML</li>
+                        <li className="flex-col justify-center items-center flex"><img src="../../public/CSS3_logo_and_wordmark.svg.png" alt="" className="w-[70px]" />CSS</li>
+                        <li className="flex-col justify-center items-center flex"><img src="../../public/Unofficial_JavaScript_logo_2.svg.png" alt="" className="w-[70px]" />JavaScript</li>
+                        <li className="flex-col justify-center items-center flex"><img src="../../public/React-icon.svg.png" alt="" className="w-[70px]" />React.js</li>
+                        <li className="flex-col justify-center items-center flex"><img src="../../public/Vue.js_Logo_2.svg.png" alt="" className="w-[70px]" />Vue.js</li>
+                        <li className="flex-col justify-center items-center flex"><img src="../../public/Tailwind_CSS_Logo.svg.png" alt="" className="w-[70px]" />TailwindCSS</li>
+                    </ul>
+                </div>
+                <div>
+                    <p>Back-End</p>
+                    <ul className="text-lg flex justify-between gap-15 mt-5">
+                        <li className="flex-col justify-center items-center flex"><img src="../../public/fastapi.svg" alt="" className="w-[70px]" />Node.js</li>
+                        <li className="flex-col justify-center items-center flex"><img src="../../public/icons8-express-js-100.png" alt="" className="w-[70px]" />Express.js</li>
+                        <li className="flex-col justify-center items-center flex"><img src="../../public/icons8-spring-boot-96.png" alt="" className="w-[70px]" />Spring Boot</li>
+                        <li className="flex-col justify-center items-center flex"><img src="../../public/icons8-mongo-db-96.png" alt="" className="w-[70px]" />MongoDB</li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
         </section>
 
