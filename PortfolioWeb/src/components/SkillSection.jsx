d79a09a9d2eb53b164a01d2005b86d8fe468767b
PhilipHousdens <philip.houden.04@gmail.com>
2025-11-12 21:15:41 +0700

    adding glowing cornner

--- PortfolioWeb/src/components/SkillSection.jsx
+++ PortfolioWeb/src/components/SkillSection.jsx
@@ -7,10 +7,23 @@ function SkillSection() {
                 <div className="w-40 h-[3px] mt-1 bg-white shadow-[0_0_20px_4px_rgba(255,255,255,0.7)]"></div>
             </div>
             <br/>
-            <div className="mt-10 flex flex-col gap-20 text-2xl ">
+            <div className="relative mt-10 flex flex-col gap-20 text-2xl ">
                 {/* Front End Section */}
                 <div>
-                    <p>Front-End</p>
+                    {/* Glowing Cornner */}
+                    <div className="absolute -top-10 -left-14 flex items-center justify-center">
+                        <div className="relative">
+                            {/* Horizontal line inner */}
+                            <div className="absolute left-0 top-1/2 w-16 h-[2px] bg-white shadow-[0_0_20px_5px_rgba(255,255,255,0.7)] -translate-y-1/2"></div>
+                            {/* Vertical line inner */}
+                            <div className="absolute left-1/2 top-0 h-16 w-[2px] bg-white shadow-[0_0_20px_5px_rgba(255,255,255,0.7)] -translate-x-1/2"></div>
+                            {/* Horizontal line Outter */}
+                            <div className="absolute left-[-20px] top-[-20px] w-30 h-[2px] bg-white shadow-[0_0_20px_5px_rgba(255,255,255,0.7)] -translate-y-1/2"></div>
+                            {/* Vertical line outter */}
+                            <div className="absolute left-[-20px] top-[-20px] h-30 w-[2px] bg-white shadow-[0_0_20px_5px_rgba(255,255,255,0.7)] -translate-x-1/2"></div>
+                        </div>
+                    </div>
+                    <p className="">Front-End</p>
                     <ul className="text-lg flex justify-between gap-15 mt-5"> 
                         <li className="flex-col justify-center items-center flex"><img src="../../public/HTML5_logo_and_wordmark.svg.png" alt="" className="h-[95px]" />HTML</li>
                         <li className="flex-col justify-center items-center flex"><img src="../../public/CSS3_logo_and_wordmark.svg.png" alt="" className="w-[70px]" />CSS</li>
@@ -28,6 +41,20 @@ function SkillSection() {
                         <li className="flex-col justify-center items-center flex"><img src="../../public/icons8-spring-boot-96.png" alt="" className="w-[70px]" />Spring Boot</li>
                         <li className="flex-col justify-center items-center flex"><img src="../../public/icons8-mongo-db-96.png" alt="" className="w-[70px]" />MongoDB</li>
                     </ul>
+
+                    {/* Glowing Cornner */}
+                    <div className="absolute -bottom-10 -right-14 flex items-center rotate-180 justify-center">
+                        <div className="relative">
+                            {/* Horizontal line inner */}
+                            <div className="absolute left-0 top-1/2 w-16 h-[2px] bg-white shadow-[0_0_20px_5px_rgba(255,255,255,0.7)] -translate-y-1/2"></div>
+                            {/* Vertical line inner */}
+                            <div className="absolute left-1/2 top-0 h-16 w-[2px] bg-white shadow-[0_0_20px_5px_rgba(255,255,255,0.7)] -translate-x-1/2"></div>
+                            {/* Horizontal line Outter */}
+                            <div className="absolute left-[-20px] top-[-20px] w-30 h-[2px] bg-white shadow-[0_0_20px_5px_rgba(255,255,255,0.7)] -translate-y-1/2"></div>
+                            {/* Vertical line outter */}
+                            <div className="absolute left-[-20px] top-[-20px] h-30 w-[2px] bg-white shadow-[0_0_20px_5px_rgba(255,255,255,0.7)] -translate-x-1/2"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
